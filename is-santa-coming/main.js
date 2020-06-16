fc88c2abd2b7e2df5eeca625b21f7ffaa8988e3f
Felix Chin <felixmchin@gmail.com>
2020-06-15 17:50:25 -0700

    updated to include month in condition

--- is-santa-coming/main.js
+++ is-santa-coming/main.js
@@ -1,5 +1,5 @@
 function isSantaComing(dateObject) {
-  if(dateObject.getDate() === 24) {
+  if(dateObject.getDate() === 24 && dateObject.getMonth() === 11) {
     return true;
   } else {
     return false;
