917005c89e782c308cad5527138def76dd6f5f13
Felix Chin <felixmchin@gmail.com>
2020-06-16 10:39:29 -0700

    free coffee cups 2

--- free-coffee-cups/main.js
+++ free-coffee-cups/main.js
@@ -2,3 +2,9 @@ function freeCoffeeCups(coffeeCups) {
   var freeCups = Math.floor(coffeeCups / 6);
   return freeCups + coffeeCups;
 }
+
+function freeCoffeeCups2(cups) {
+  var remainder = cups % 6;
+  var freeCups = (cups - remainder) / 6;
+  return cups + freeCups;
+}
