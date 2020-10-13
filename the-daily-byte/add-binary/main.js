34bdf002ebff780048a73accc840f4e88fa11093
Felix Chin <felixmchin@gmail.com>
2020-10-13 00:01:36 -0700

    daily byte

---
+++ the-daily-byte/add-binary/main.js
@@ -0,0 +1,15 @@
+const addBinary = (str1, str2) => {
+  const map = {};
+  let position1 = 1;
+  let position2 = 1;
+  let carryover = null;
+  for (let i = str1.length - 1; i >= 0; i--) {
+    map[position1] = str1[i];
+    position1++;
+  }
+  for (let i = str2.length - 1; i >= 0 ; i--) {
+    if (position2 === map[position2]) {
+      if ()
+    }
+  }
+}
