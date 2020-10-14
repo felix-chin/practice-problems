88f2a51b91ab2a2758f582597a0a8c0237f8304d
Felix Chin <felixmchin@gmail.com>
2020-10-13 22:16:23 -0700

    daily byte

--- the-daily-byte/longest-common-prefix/main.js
+++ the-daily-byte/longest-common-prefix/main.js
@@ -1,12 +1,36 @@
-const longestCommonPrefix = (arr) => {
-  let prefix = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i].indexOf(prefix) === -1 || arr[i].indexOf(prefix) !== 0) {
+// Horizontal scanning
+// time: O(S) where S is the sum of all characters in a string
+// space: O(1)
+const longestCommonPrefix1 = (strs) => {
+  if (strs.length === 0) {
+    return '';
+  }
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    if (strs[i].indexOf(prefix) !== 0) {
       prefix = prefix.slice(0, -1);
       i--;
     }
-    console.log(prefix);
   }
+  return prefix;
+}
 
-  return screen.length === 0 ? '' : prefix;
+// Vertical scanning
+// time: O(S) where S is the sum of all characters in a string.
+//  In the worst case there will be nn equal strings with length mm and the algorithm performs S = m \cdot nS=m⋅n character comparisons.
+//  Even though the worst case is still the same as Approach 1, in the best case there are at most n \cdot minLenn⋅minLen comparisons where minLenminLen is the length of the shortest string in the array.
+// space: O(1)
+const longestCommonPrefix2 = (strs) => {
+  if (strs === null || strs.length === 0) {
+    return '';
+  }
+  for (let i = 0; i < strs[0].length; i++) {
+    const c = strs[0].charAt(i);
+    for (let j = 1; j < strs.length; j++) {
+      if (i === strs[j].length || strs[j].charAt(i) != c) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+  return strs[0];
 }
