bc6737eae62eb3d546be1e6b0afa8532f3fec7f9
Felix Chin <felixmchin@gmail.com>
2020-10-15 23:14:20 -0700

    daily byte

--- the-daily-byte/jewels-and-stones/main.js
+++ the-daily-byte/jewels-and-stones/main.js
@@ -1,10 +1,13 @@
 const jewelsAndStones = (jewels, stones) => {
   const map = {};
   let count = 0;
-  for (let i = 0; i < stones.length; i++) {
-    map[stones[i]] = i;
-  }
   for (let i = 0; i < jewels.length; i++) {
-
+    map[jewels[i]] = i;
   }
+  for (let i = 0; i < stones.length; i++) {
+    if (map.hasOwnProperty(stones[i])) {
+      count++;
+    }
+  }
+  return count;
 }
