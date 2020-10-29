f7ad480cfaf765210837277954b7f381e1785242
Felix Chin <felixmchin@gmail.com>
2020-10-29 14:44:26 -0700

    add arrays left rotation

--- hackerrank/arrays-left-rotation/main.js
+++ hackerrank/arrays-left-rotation/main.js
@@ -1,13 +1,13 @@
-function hourglassSum(arr) {
-  let max = -Infinity;
-  for (let i = 0; i <= 3; i++) {
-    for (let j = 0; j <= 3; j++) {
-      const sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1]
-        + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
-      if (sum > max) {
-        max = sum;
-      }
-    }
+function rotLeft(a, d) {
+  if (d === 0) {
+    return a;
   }
-  return max;
+
+  const result = a.slice(0);
+
+  for (let i = 0; i < d; i++) {
+    result.push(result.shift());
+  }
+
+  return result;
 }
