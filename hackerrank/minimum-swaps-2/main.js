c8ec4bc51ea2d985ba5bce3d857ec11b7f2df863
Felix Chin <felixmchin@gmail.com>
2020-10-29 23:18:54 -0700

    added minimum swaps 2

--- hackerrank/minimum-swaps-2/main.js
+++ hackerrank/minimum-swaps-2/main.js
@@ -1,7 +1,11 @@
 function minimumSwaps(arr) {
+  let placeholder;
   let mid = Math.floor(arr.length / 2)
   for (let i = 0; i < arr.length; i++) {
     if (arr[0] > arr[mid]) {
+      placeholder = arr[mid];
+      arr[mid] = arr[0];
+      arr[0] = placeholder;
     }
   }
 }
