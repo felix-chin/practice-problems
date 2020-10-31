e807c4fc0723ff5dadfc8d63b9fcb1b48757d2a8
Felix Chin <felixmchin@gmail.com>
2020-10-31 13:35:12 -0700

    hash tables ransom note

--- hackerrank/array-manipulation/main.js
+++ hackerrank/array-manipulation/main.js
@@ -1,8 +1,4 @@
 function checkMagazine(magazine, note) {
-  if (magazine.length < note.length) {
-    console.log('No');
-    return;
-  }
   const map = {};
   for (let i = 0; i < magazine.length; i++) {
     if (map[magazine[i]]) {
@@ -12,11 +8,11 @@ function checkMagazine(magazine, note) {
     }
   }
   for (let i = 0; i < note.length; i++) {
-    if (!map.hasOwnProperty(note[i]) || map[note[i]] === 0) {
+    if (map[note[i]] > 0) {
+      map[note[i]]--;
+    } else {
       console.log('No');
       return;
-    } else {
-      map[note[i]]--;
     }
   }
   console.log('Yes');
