d66919140a3d99f40989f80209001b08a9d374a9
Felix Chin <felixmchin@gmail.com>
2020-10-31 13:30:38 -0700

    hash tables ransom note

--- hackerrank/array-manipulation/main.js
+++ hackerrank/array-manipulation/main.js
@@ -1,45 +1,24 @@
-// Brute Force solution
-// time: O(n^2)
-// space: probably O(n^2) from 2D queries array
-function arrayManipulation(n, queries) {
-  const result = [];
-  let maxValue = -Infinity;
-  for (let i = 0; i < n; i++) {
-    result.push(0);
+function checkMagazine(magazine, note) {
+  if (magazine.length < note.length) {
+    console.log('No');
+    return;
   }
-  for (let i = 0; i < queries.length; i++) {
-    for (let j = queries[i][0] - 1; j < queries[i][1]; j++) {
-      result[j] += queries[i][2];
-      if (result[j] > maxValue) {
-        maxValue = result[j];
-      }
+  const map = {};
+  for (let i = 0; i < magazine.length; i++) {
+    if (map[magazine[i]]) {
+      map[magazine[i]]++;
+    } else {
+      map[magazine[i]] = 1;
     }
   }
-  return maxValue;
-}
-
-// Calculate the rate of change (slope)
-// return where sum of rate of change is largest value
-// time: O(n)
-// space: probably O(n^2) from 2D queries array
-function arrayManipulation(n, queries) {
-  const result = [];
-  let max = 0;
-  let sum = 0;
-  for (let i = 0; i < n; i++) {
-    result.push(0);
-  }
-  for (const query of queries) {
-    result[query[0] - 1] += query[2];
-    if (result[query[1]] !== undefined) {
-      result[query[1]] += -query[2];
+  for (let i = 0; i < note.length; i++) {
+    if (!map.hasOwnProperty(note[i]) || map[note[i]] === 0) {
+      console.log('No');
+      return;
+    } else {
+      map[note[i]]--;
     }
   }
-  for (let i = 0; i < result.length; i++) {
-    sum += result[i];
-    if (sum > max) {
-      max = sum;
-    }
-  }
-  return max;
+  console.log(map)
+  console.log('Yes');
 }
