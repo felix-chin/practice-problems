55a7b1466506a8bc2bece90fab25648eb42fed2b
Felix Chin <felixmchin@gmail.com>
2020-10-31 00:30:02 -0700

    array manipulation

--- hackerrank/array-manipulation/main.js
+++ hackerrank/array-manipulation/main.js
@@ -14,3 +14,30 @@ function arrayManipulation(n, queries) {
   }
   return maxValue;
 }
+
+function arrayManipulation(n, queries) {
+  const result = [];
+  let globalMax = 0;
+  let currentMax = 0;
+  for (let i = 0; i < n; i++) {
+    result.push(0);
+  }
+  for (let i = 0; i < queries.length; i++) {
+    result[queries[i][0] - 1] += queries[i][2];
+    if (result[queries[i][1]] !== undefined) {
+      result[queries[i][1]] += -queries[i][2];
+    }
+  }
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] < 0) {
+      currentMax = 0;
+    }
+    if (result[i] + currentMax > currentMax) {
+      currentMax = result[i] + currentMax;
+    }
+    if (currentMax > globalMax) {
+      globalMax = currentMax;
+    }
+  }
+  return globalMax;
+}
