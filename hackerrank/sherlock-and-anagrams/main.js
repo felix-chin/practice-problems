2f67a64880d3134b8bad1f7f7077368201b09141
Felix Chin <felixmchin@gmail.com>
2020-11-01 20:26:46 -0800

    sherlock and anagrams

--- hackerrank/sherlock-and-anagrams/main.js
+++ hackerrank/sherlock-and-anagrams/main.js
@@ -1,20 +1,27 @@
+// Brute Force
+// 1. Iterate through string to find all possible substrings
+// 2. sort substrings and then store in hash map with substring as key and increment value based on occurences
+// 3. if new substring, add to hash map, otherwise increment counter by number of occurences
+// 4.  (do not increment counter by one, increment hash map occurences and then increase counter by number of occurences when encountered)
+// space: O(n^2)
+// time: O(n)
+
 function sherlockAndAnagrams(s) {
   const map = {};
   let count = 0;
   for (let i = 0; i < s.length; i++) {
+    let subStr = '';
     let mapStr = '';
     for (let j = i; j < s.length; j++) {
-      mapStr += s[j];
+      subStr += s[j];
+      mapStr = subStr.split('').sort().join('');
       if (map.hasOwnProperty(mapStr)) {
+        counter += map[mapStr];
         map[mapStr]++;
-      } else if (mapStr.length !== s.length) {
+      } else {
         map[mapStr] = 1;
       }
     }
   }
-  for (key in map) {
-    if (key.length)
-  }
-  console.log(map);
   return count;
 }
