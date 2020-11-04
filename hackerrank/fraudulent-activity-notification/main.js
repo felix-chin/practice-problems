fbae540f48febe5ad0f60891504fae91b7c4b820
Felix Chin <felixmchin@gmail.com>
2020-11-03 18:32:27 -0800

    fraud activity notification

--- hackerrank/fraudulent-activity-notification/main.js
+++ hackerrank/fraudulent-activity-notification/main.js
@@ -1,27 +1,33 @@
 // time:
 // space:
+const arr = [100,2,40,0,31,65,8,40,5];
 function activityNotifications(expenditure, d) {
-  let notifications = 0;
-  const counts = [];
-  const sorted = [];
-  let queue = expenditure.slice(0, d);
-  for (let i = 0; i < expenditure.length; i++) {
-    counts[expenditure[i]] !== undefined ? counts[expenditure[i]]++ : counts[expenditure[i]] = 1;
-  }
-  for (let i = 1; i < counts.length; i++) {
-    counts[i] += counts[i - 1];
+  debugger;
+  let n = 0;
+  let [i1, i2] = [ Math.floor((d - 1) / 2), Math.ceil((d - 1) / 2)];
+  let m1, m2, m;
+  let countSorted = new Array(201).fill(0);
+  for (let i = d - 1; i >= 0; i--) {
+    countSorted[expenditure[i]]++;
   }
   for (let i = d; i < expenditure.length; i++) {
-
-    let median;
-    if (d % 2 === 0) {
-      median = (spending[d / 2 - 1] + spending[d / 2]) / 2;
-    } else {
-      median = spending[Math.ceil(d / 2) - 1]
+    // Find Median:
+    // since d is the maximum number of elements that will be considered, we know the median will be half of the maximum
+    // Example: if d = 5, there are 5 elements. in a sorted array, the 3rd element will be the median.
+    // since count sorting sorts elements innately using the index, we can derive the median by
+    // by iterating through the index of the count sorted array and counting occurrences until we reach the mid count.
+    for (let j = 0, k = 0; k <= i1; k += countSorted[j], j++) {
+      m1 = j;
     }
-    if (expenditure[i] >= median * 2) {
-      notifications++;
+    for (let j = 0, k = 0; k <= i2; k += countSorted[j], j++) {
+      m2 = j;
     }
+    let m = (m1 + m2) / 2;
+    if (expenditure[i] >= m * 2) {
+      n++;
+    }
+    countSorted[expenditure[i - d]]--;
+    countSorted[expenditure[i]]++;
   }
-  return notifications;
+  return n;
 }
