a6403fb2df1b6e01eac5681f3e4caea2836c4456
Felix Chin <felixmchin@gmail.com>
2020-11-04 17:37:15 -0800

    sherlock and the valid string

--- hackerrank/sherlock-and-the-valid-string/main.js
+++ hackerrank/sherlock-and-the-valid-string/main.js
@@ -1,11 +1,33 @@
-// time: O(n)
-// space: O(1)
-function alternatingCharacters(s) {
-  let count = 0;
-  for (let i = 1; i < s.length; i++) {
-    if (s[i] === s[i - 1]) {
-      count++;
-    }
+function isValid(s) {
+  const map = {};
+  const freq = {};
+  for (let i = 0; i < s.length; i++) {
+    map[s[i]] = map[s[i]] || 0;
+    map[s[i]]++;
   }
-  return count;
+
+  for (const key in map) {
+    freq[map[key]] = freq[map[key]] || 0;
+    freq[map[key]]++;
+  }
+
+  let freqArr = Object.keys(freq).map(Number)
+
+  if (freqArr.length === 1) {
+    return 'YES';
+  }
+  let twoFreq = freqArr.length === 2;
+  let [a, b] = freqArr;
+
+  let oneOccurrence = freq[a] === 1 || freq[b] === 1;
+
+  let singleton = (freq[a] === 1 && a === 1) || (freq[b] === 1 && b === 1);
+
+  let diffOfOne = (freq[a] === 1 ? a - b : b - a) === 1;
+
+  if ((singleton || diffOfOne) && twoFreq && oneOccurence) {
+    return 'YES'
+  }
+
+  return 'NO';
 }
