235e8f20d720aff747c878fe75d0f5239c903f31
Felix Chin <felixmchin@gmail.com>
2020-11-23 23:44:03 -0800

    concatenatedSum

--- codesignal/concatenatedSum/main.js
+++ codesignal/concatenatedSum/main.js
@@ -1,21 +1,11 @@
 const concatenatedSum = (a) => {
-  const result = [];
+  let sum = 0;
+  let offset = 0;
   for (let i = 0; i < a.length; i++) {
-    const str = a[i].toString() + a[i].toString();
-    result.push(parseInt(str))
+    sum += a[i];
+    const size = a[i].toString().length;
+    offset += Math.pow(10, size);
   }
-  for (let i = 0; i < Math.ceil(a.length / 2); i++) {
-    let str1 = '';
-    let str2 = '';
-    for (let j = 1; j < a.length; j++) {
-      str1 = a[i].toString() + a[j].toString();
-      str2 = a[j].toString() + a[i].toString();
-      result.push(parseInt(str1), parseInt(str2))
-    }
-  }
-  console.log(result);
+  return sum * a.length + sum * offset;
 }
-
-concatenatedSum([1, 2, 3, 4])
-11 + 12 + 13 + 21 + 22 + 23 + 31 + 32 + 33
-11 + 12 + 13 + 14 + 21 + 22 + 23 + 24+ 31 + 32 + 33 + 34 + 41 + 42 + 43 + 44
+concatenatedSum([10, 2])
