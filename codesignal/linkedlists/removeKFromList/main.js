197505df8f15770c98b25bcaeb714ba9cb774be2
Felix Chin <felixmchin@gmail.com>
2020-11-26 23:30:40 -0800

    linkedlists

--- codesignal/linkedlists/removeKFromList/main.js
+++ codesignal/linkedlists/removeKFromList/main.js
@@ -5,14 +5,21 @@
 // }
 //
 function removeKFromList(l, k) {
-  if (l == null) {
-    return null;
+  if (l === null) {
+    return l;
+  }
+  while (l.value == k) {
+    l = l.next;
   }
   let thisNode = l;
   let nextNode = thisNode.next;
   while (nextNode !== null) {
-    if (next.value === k) {
+    if (nextNode.value === k) {
       thisNode.next = nextNode.next;
+      if (thisNode.next === null) break;
     }
+    thisNode = thisNode.next;
+    nextNode = thisNode.next;
   }
+  return l;
 }
