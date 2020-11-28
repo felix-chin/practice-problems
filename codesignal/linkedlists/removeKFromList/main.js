9c08ed5d868e6cd35d6608dfac945f211866c008
Felix Chin <felixmchin@gmail.com>
2020-11-27 17:16:59 -0800

    removeKFromList

--- codesignal/linkedlists/removeKFromList/main.js
+++ codesignal/linkedlists/removeKFromList/main.js
@@ -8,18 +8,24 @@ function removeKFromList(l, k) {
   if (l === null) {
     return l;
   }
-  while (l.value == k) {
-    l = l.next;
+  while (l.value === k) {
+    if (l.next === null) {
+      return null;
+    } else {
+      l = l.next;
+    }
   }
   let thisNode = l;
-  let nextNode = thisNode.next;
-  while (nextNode !== null) {
-    if (nextNode.value === k) {
-      thisNode.next = nextNode.next;
-      if (thisNode.next === null) break;
+  while (thisNode.next !== null) {
+    if (thisNode.next.value === k) {
+      thisNode.next = thisNode.next.next;
+      if (thisNode.next === null) {
+        break;
+      }
     }
-    thisNode = thisNode.next;
-    nextNode = thisNode.next;
+    if (thisNode.next.value !== k) {
+      thisNode = thisNode.next;
+    }
   }
   return l;
 }
