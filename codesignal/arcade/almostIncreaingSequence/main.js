8b8cae6dcab7749ad8d1fc00deacdf4251620d3e
Felix Chin <felixmchin@gmail.com>
2020-12-03 23:57:06 -0800

    almostIncreasingSequence

--- codesignal/arcade/almostIncreaingSequence/main.js
+++ codesignal/arcade/almostIncreaingSequence/main.js
@@ -2,13 +2,17 @@ function almostIncreasingSequence(sequence) {
   if (sequence.length === 1) {
     return true;
   }
-  let counter = 0;
-  for (let i = 1; i < sequence.length; i++) {
-    if (sequence[i] < sequence[i - 1]) {
-      counter++
-    }
-    if (sequence[i] <= sequence[i - 1] && counter > 1) {
-      return false;
+  for (let i = 0; i < sequence.length - 1; i++) {
+    if (sequence[i] >= sequence[i + 1]) {
+      let count = 0;
+      for (let j = i + 1; j < sequence.length; j++) {
+        if (sequence[j] < sequence[i]) {
+          count++;
+        }
+        if (count > 1) {
+          return false;
+        }
+      }
     }
   }
   return true;
