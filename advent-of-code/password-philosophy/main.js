d0cf1fdd070de721c21c17652ec9120d46b60b32
Felix Chin <felixmchin@gmail.com>
2020-12-15 20:57:12 -0800

    password philosophy part 1

--- advent-of-code/password-philosophy/main.js
+++ advent-of-code/password-philosophy/main.js
@@ -8,13 +8,23 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
     return el.split(' ');
   }).map(el2 => {
     const i0 = el2[0].split('-');
-    const i1 = {};
-    i1[el2[1][0]] = el2[2]
-    return [[parseInt(i0[0]), parseInt(i0[1])], i1];
+    return [parseInt(i0[0]), parseInt(i0[1]), el2[1][0], el2[2]];
   })
-  console.log(arr)
+  passwordPhilosophy(arr);
 })
 
-// const passwordPhilosophy = () => {
-
-// }
+const passwordPhilosophy = (list) => {
+  let res = 0;
+  for (let i = 0; i < list.length; i++) {
+    let countLetter = 0;
+    for (let j = 0; j < list[i][3].length; j++) {
+      if (list[i][3][j] === list[i][2]) {
+        countLetter++;
+      }
+    }
+    if (countLetter >= list[i][0] && countLetter <= list[i][1]) {
+      res++;
+    }
+  }
+  console.log(res)
+}
