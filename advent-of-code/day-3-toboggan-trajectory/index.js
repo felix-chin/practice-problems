c0cd6424292bdff9356d60184faa11a0b0becbc1
Felix Chin <felixmchin@gmail.com>
2020-12-15 23:32:27 -0800

    day 3 pt 2

--- advent-of-code/day-3-toboggan-trajectory/index.js
+++ advent-of-code/day-3-toboggan-trajectory/index.js
@@ -5,7 +5,7 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
     console.error(err);
   }
   const arr = data.split('\n');
-  tobogganTrajectory1(arr);
+  console.log(tobogganTrajectory1(arr) * tobogganTrajectory2(arr));
 })
 
 const tobogganTrajectory1 = (map) => {
@@ -20,5 +20,36 @@ const tobogganTrajectory1 = (map) => {
       j = j - map[i].length;
     }
   }
-  console.log(res)
+  console.log('pt1', res);
+  return res;
+}
+
+const tobogganTrajectory2 = (map) => {
+  let count1 = 0;
+  let count2 = 0;
+  let count3 = 0;
+  let count4 = 0;
+  let j1 = 0;
+  let j2 = 0;
+  let j3 = 0;
+  let j4 = 0;
+  for (let i = 0; i < map.length; i++) {
+    map[i][j1] === '#' && count1++;
+    map[i][j2] === '#' && count2++;
+    map[i][j3] === '#' && count3++;
+    j1++;
+    j2 += 5;
+    j3 += 7;
+    if (j1 > map[i].length - 1) j1 = j1 - map[i].length;
+    if (j2 > map[i].length - 1) j2 = j2 - map[i].length;
+    if (j3 > map[i].length - 1) j3 = j3 - map[i].length;
+    if (i % 2 === 0) {
+      map[i][j4] === '#' && count4++;
+      j4 += 1;
+      if (j4 > map[i].length - 1) j4 = j4 - map[i].length;
+    }
+  }
+  let res = count1 * count2 * count3 * count4;
+  console.log('pt2', res);
+  return res;
 }
