2204218e9f057039a56f9985c3e0b095b9e426c9
Felix Chin <felixmchin@gmail.com>
2020-12-16 23:54:47 -0800

    advent of code day 4

--- advent-of-code/day-4-passport-processing/index.js
+++ advent-of-code/day-4-passport-processing/index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 fs.readFile('./input.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
-  }
+  };
   const arr = data.split('\n').flatMap(val => {
     return val.split(' ');
   });
