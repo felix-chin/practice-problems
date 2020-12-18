3403ec492256c78bb31e09998a9f09c5fa90b942
Felix Chin <felixmchin@gmail.com>
2020-12-17 23:17:15 -0800

    advent of code day 4

--- advent-of-code/day-4-passport-processing/index.js
+++ advent-of-code/day-4-passport-processing/index.js
@@ -11,5 +11,47 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
 })
 
 const passportProcessing = (passports) => {
-  console.log(passports);
+  let validPassports = 0;
+  const set = new Set();
+
+  const addToSet = () => {
+    set.add('byr');
+    set.add('iyr');
+    set.add('pid');
+    set.add('eyr');
+    set.add('ecl');
+    set.add('hcl');
+    set.add('hgy')
+  }
+
+  for (let i = 0; i < passports.length; i++) {
+    if (passports[i] === '') {
+      if (set.size === 0) {
+        validPassports++;
+      }
+      addToSet();
+    }
+    switch(passports[i].slice(0, 3)) {
+      case 'byr':
+        set.delete('byr');
+        break;
+      case 'iyr':
+        set.delete('iyr');
+        break;
+      case 'pid':
+        set.delete('pid');
+        break;
+      case 'eyr':
+        set.delete('eyr');
+        break;
+      case 'ecl':
+        set.delete('ecl');
+        break;
+      case 'hcl':
+        set.delete('hcl');
+        break;
+      case 'hgy':
+        set.delete('hgy');
+    }
+  }
 }
