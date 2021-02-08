86ec1ded34daaa60440e774d65c7c3b729157a1a
Felix Chin <felixmchin@gmail.com>
2021-02-07 18:06:05 -0800

    codility - updated maxCounters

--- codiliity/max-counters/index.js
+++ codiliity/max-counters/index.js
@@ -1,16 +1,21 @@
 function solution(N, A) {
   // write your code in JavaScript (Node.js 8.9.4)
-  const result = new Array(N).fill(0);
-  let max = 0;
+  let counters = new Array(N).fill(0);
+  let maxCounter = 0;
+  let maxToSet = 0;
+
   for (let i = 0; i < A.length; i++) {
-    if (A[i] === N + 1) {
-      result.fill(max);
+    const X = A[i] - 1;
+
+    if (X === N) {
+      maxToSet = maxCounter;
     } else {
-      result[A[i] - 1]++;
-      if (result[A[i] - 1] > max) {
-        max = result[A[i] - 1];
-      }
+      counters[X] = Math.max(counters[X] + 1, maxToSet + 1);
+      maxCounter = Math.max(counters[X], maxCounter);
     }
   }
-  return result;
+
+  counters = counters.map(val => Math.max(val, maxToSet));
+
+  return counters;
 }
