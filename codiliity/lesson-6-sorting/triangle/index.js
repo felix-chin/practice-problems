57e26068ec58f3124cfb0c1e067252fcd649bc5d
Felix Chin <felixmchin@gmail.com>
2021-02-09 22:17:15 -0800

    codility triangle

---
+++ codiliity/lesson-6-sorting/triangle/index.js
@@ -0,0 +1,15 @@
+function solution(A) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  if (A.length < 3) {
+    return 0;
+  }
+
+  A.sort((a, b) => a - b);
+
+  for (let i = 0; i < A.length - 2; i++) {
+    if (A[i] >= 0 && A[i] + A[i + 1] > A[i + 2]) {
+      return 1;
+    }
+  }
+  return 0;
+}
