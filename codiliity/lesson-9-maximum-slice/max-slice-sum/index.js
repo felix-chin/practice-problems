2c79cfc92d081efa5889d4318c95ff316e98d524
Felix Chin <felixmchin@gmail.com>
2021-02-11 22:43:41 -0800

    codility maxSliceSum

--- codiliity/lesson-9-maximum-slice/max-slice-sum/index.js
+++ codiliity/lesson-9-maximum-slice/max-slice-sum/index.js
@@ -1,7 +1,7 @@
 function solution(A) {
   // write your code in JavaScript (Node.js 8.9.4)
   let maxEnding = 0;
-  let maxSlice = A[0];
+  let maxSlice = -Infinity;
   for (const a of A) {
     maxEnding = Math.max(a, maxEnding + a);
     maxSlice = Math.max(maxSlice, maxEnding);
